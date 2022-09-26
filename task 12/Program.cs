e7a438ab1967eff468d777f882bc91e73a888015
Lina <datsenkolina99@gmail.com>
2022-09-26 17:09:04 +0300

    сделали 12 задачу

--- task 12/Program.cs
+++ task 12/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string numText= Convert.ToString(num);
+if (numText.Length > 2)
+{
+    Console.WriteLine($"Третья цифра - > {numText[2]}");
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
