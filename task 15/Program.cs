8e379677cc9258ea92d07c50b7a52e8bf0051eba
Lina <datsenkolina99@gmail.com>
2022-09-26 17:23:31 +0300

    доделали второе дз

--- task 15/Program.cs
+++ task 15/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите цифру обозначающую день недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7)
+{
+    Console.WriteLine("Это выходной день");
+}
+else if (num < 1 || num > 7)
+{
+    Console.WriteLine("Это вообще не день недели");
+}
+else 
+{
+    Console.WriteLine("Это не выходной день");
+}
