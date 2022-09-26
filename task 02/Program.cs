a2bed20adbcff73c74afa1fa40652e6f28cd3346
Lina <datsenkolina99@gmail.com>
2022-09-26 15:27:47 +0300

    Решили задачу 246

---
+++ task 02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.Write($"Большее число из пары {number1}");
+}
+else Console.Write($"Большее число из пары {number2}");
