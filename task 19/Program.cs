1018d895534a134f09f0e5cc49cc5eda356316f6
Lina <datsenkolina99@gmail.com>
2022-10-02 16:18:58 +0300

    19 ready

--- task 19/Program.cs
+++ task 19/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+if (len == 5)
+{
+
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"Ошибка: {number} - не является пятизначным");
+}
