26018993cb89c5f6b1e5664957a9b1b19048c6d3
Lina <datsenkolina99@gmail.com>
2022-10-02 16:41:33 +0300

    ready 21

--- task 21/Program.cs
+++ task 21/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x1 = ReadInt ("Введите координату Х первой точки: ");
+int y1 = ReadInt ("Введите координату У первой точки: ");
+int z1 = ReadInt ("Введите координату Z первой точки: ");
+int x2 = ReadInt ("Введите координату Х второй точки: ");
+int y2 = ReadInt ("Введите координату Y второй точки: ");
+int z2 = ReadInt ("Введите координату Z второй точки: ");
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+double lenght = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длина отрезка {lenght}");
+// Функция ввода сообщения
+int ReadInt (string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
