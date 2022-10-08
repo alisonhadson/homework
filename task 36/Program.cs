40d1f224c96f095561d3477ab6e16dacd82a03c2
Lina <datsenkolina99@gmail.com>
2022-10-08 16:23:48 +0300

    finish 36 ewe raz

--- task 36/Program.cs
+++ task 36/Program.cs
@@ -5,7 +5,7 @@ FillArrayRandomNumbers(numbers);
 Console.WriteLine("Вот наш массив: ");
 PrintArray(numbers);
 int sum = 0;
-for (int z = 0; z < numbers.Length; z += 2)
+for (int z = 1; z < numbers.Length; z += 2)
     sum = sum + numbers[z];
 Console.WriteLine( $"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
 void FillArrayRandomNumbers(int[] numbers)
